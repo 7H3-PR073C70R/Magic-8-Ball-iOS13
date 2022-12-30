bd38035ded8c3f3200bc7926e216b6626245e540
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2022-12-30 14:11:17 +0200

    task completed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBAction func button(_ sender: UIButton) {
+        imageView.animationImages = ballArray
+        imageView.animationDuration = TimeInterval(1)
+        imageView.startAnimating()
+        Task {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            imageView.stopAnimating()
+            imageView.image = ballArray[Int.random(in: 0...4)]
+        }
+       
+    }
+    
 }
 
